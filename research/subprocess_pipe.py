60f0c3a252cec4f3fe274d7808a06d7845e0c7c1
Peter Bakhirev <pbkhrv@pm.me>
2019-10-01 21:57:53 -0400

    cleanup

--- research/subprocess_pipe.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-cp = subprocess.run(["keepassxc-cli", "locate", "-q", "/home/peter/SynologyDrive/passwords/Passwords.kdbx", "amex"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=b"passwordhere")
-
-print(cp.returncode)
-print(cp.stdout)
-print(cp.stderr)
