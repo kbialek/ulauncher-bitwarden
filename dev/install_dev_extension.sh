52bbfbcc1eed9ef8c1b0ba6c0a9e7f01ced9fc0b
Peter Bakhirev <pbkhrv@pm.me>
2019-10-03 22:16:15 -0400

    dev helpers: test db, script to install dev extension in ulauncher

---
+++ dev/install_dev_extension.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+set -e
+
+EXTENSION_ID=com.github.pbkhrv.ulauncher-keepassxc
+ULAUNCHER_EXT_DIR=~/.local/share/ulauncher/extensions/
+
+# Remove whatever version of this extension is installed
+rm -rf ${ULAUNCHER_EXT_DIR}/${EXTENSION_ID}
+
+# Symlink current dir if extension is here
+if [ -e "manifest.json" ]
+then
+	ln -s $(pwd) ${ULAUNCHER_EXT_DIR}/${EXTENSION_ID}
+	echo "Done"
+else
+	echo "Please run this script from the root directory of the extension"
+fi
